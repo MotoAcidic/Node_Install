887522610e620cfd8a471778f57c87b33af27a84
Tfinch <travisfinch01@gmail.com>
2019-09-10 20:00:49 -0400

    Add Altbet

---
+++ Coins/Altbet/install.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+###################################################################################################
+# COPY THIS FILE AND CHANGE ONLY THE SPECS BELOW FOR YOUR COIN                                    #
+# Must use exact repo name: Example github https://github.com/altbet Example repo name: altbet    #
+###################################################################################################
+RPC_PORT=9322
+COIN_PORT=8322
+COIN_NAME='Altbet'
+REPO_NAME='altbet'
+COIN_DAEMON='altbetd'
+COIN_CLI='altbet-cli'
+GITHUB='https://github.com/altbet/abet'
+
+###############
+# Dont Change #
+###############
+DEPENDS="/Node_Install/Depends/install.sh"
+EXTIP=`curl -s4 icanhazip.com`
+RPCUSER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+RPCPASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+COIN_PATH=.$REPO_NAME/
+
+###################
+# Install Depends #
+###################
+bash "$DEPENDS"
+clear
+echo VPS Server prerequisites installed.
+
+#################################
+# Make sure firewall is opened. #
+#################################
+  echo -e "Installing and setting up firewall to allow ingress on port ${GREEN}$COIN_PORT${NC}"
+  ufw allow $COIN_PORT/tcp comment "$COIN_NAME MN port" >/dev/null
+  ufw allow ssh comment "SSH" >/dev/null 2>&1
+  ufw limit ssh/tcp >/dev/null 2>&1
+  ufw default allow outgoing >/dev/null 2>&1
+  echo "y" | ufw enable >/dev/null 2>&1
+
+####################
+# Compile the Coin #
+####################
+git clone $GITHUB
+cd $REPO_NAME
+./autogen.sh
+./configure --with-incompatible-bdb --disable-tests --with-gui=no
+make install
+cd
+
+##############################
+# Masternode Genkey Creation #
+##############################
+  echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
+  read -e COINKEY
+  if [[ -z "$COINKEY" ]]; then
+  $COIN_PATH$COIN_DAEMON -daemon
+  sleep 30
+  if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
+   echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
+   exit 1
+  fi
+  COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
+  if [ "$?" -gt "0" ];
+    then
+    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
+    sleep 30
+    COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
+  fi
+  $COIN_PATH$COIN_CLI stop
+fi
+clear
+
+##########################
+# Create the Config file #
+##########################
+mkdir $COIN_PATH
+echo listen=1 > $COIN_PATH/$REPO_NAME.conf
+echo server=1 >> $COIN_PATH/$REPO_NAME.conf
+echo daemon=1 >> $COIN_PATH/$REPO_NAME.conf
+echo staking=0 >> $COIN_PATH/$REPO_NAME.conf
+echo rpcuser=testuser >> $COIN_PATH/$REPO_NAME.conf
+echo rpcpassword=testpassword >> $COIN_PATH/$REPO_NAME.conf
+echo rpcallowip=127.0.0.1 >> $COIN_PATH/$REPO_NAME.conf
+echo rpcbind=127.0.0.1 >> $COIN_PATH/$REPO_NAME.conf
+echo maxconnections=24 >> $COIN_PATH/$REPO_NAME.conf
+echo masternode=1 >> $COIN_PATH/$REPO_NAME.conf
+echo masternodeprivkey=$COINKEY >> $COIN_PATH/$REPO_NAME.conf
+echo bind=$EXTIP >> $COIN_PATH/$REPO_NAME.conf
+echo externalip=$EXTIP >> $COIN_PATH/$REPO_NAME.conf
+echo masternodeaddr=$EXTIP:$COIN_PORT >> $COIN_PATH/$REPO_NAME.conf
+
+##################
+# Run the daemon #
+##################
+$COIN_DAEMON
+
+watch $COIN_PATH$COIN_CLI
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-Node_Install
+## Node Install 
+This script will let you setup your own VPN server in no more than a minute, even if you haven't used configured a vpn before. It has been designed to be as unobtrusive and universal as possible.
+
+### Installation
+Run the script and follow the assistant for the coin you wish to install:
+
+```
+git clone https://github.com/MotoAcidic/Node_Install.git
+```
+
+```
+cd Node_Install
+```
+
+```
+bash Install.sh
+```
+
+### All documents
+All guides and help documents can be found here: [Documentation](https://github.com/MotoAcidic/Node_Install/Docs/)
+
+**Have fun, this is crypto after all!**
+
+```
+BTC  1MaxQQxJjnrxxfVvPncb2wsVpTKQu1drH7
+```
+
+Feel free to use my reflink to signup and receive a bonus w/ vultr:
+<a href="https://www.vultr.com/?ref=6903922"><img src="https://www.vultr.com/media/banner_2.png" width="468" height="60"></a>
