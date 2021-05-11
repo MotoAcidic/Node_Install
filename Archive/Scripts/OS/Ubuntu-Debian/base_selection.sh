f9c63c1b38ff181c423828461a6dede8a524e9a6
TFinch <travisfinch01@gmail.com>
2021-05-11 18:18:53 -0400

    full rework phase 1

---
+++ Archive/Scripts/OS/Ubuntu-Debian/base_selection.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+# Copyright (c) 2019 - 2020 Node_Install. Released under the MIT License.
+
+# .-----------------. .----------------.  .----------------.  .----------------.                                                             
+#| .--------------. || .--------------. || .--------------. || .--------------. |                                                            
+#| | ____  _____  | || |     ____     | || |  ________    | || |  _________   | |                                                            
+#| ||_   \|_   _| | || |   .'    `.   | || | |_   ___ `.  | || | |_   ___  |  | |                                                            
+#| |  |   \ | |   | || |  /  .--.  \  | || |   | |   `. \ | || |   | |_  \_|  | |                                                            
+#| |  | |\ \| |   | || |  | |    | |  | || |   | |    | | | || |   |  _|  _   | |                                                            
+#| | _| |_\   |_  | || |  \  `--'  /  | || |  _| |___.' / | || |  _| |___/ |  | |                                                            
+#| ||_____|\____| | || |   `.____.'   | || | |________.'  | || | |_________|  | |                                                            
+#| |              | || |              | || |              | || |              | |                                                            
+#| '--------------' || '--------------' || '--------------' || '--------------' |                                                            
+# '----------------'  '----------------'  '----------------'  '----------------'                                                             
+# .----------------.  .-----------------. .----------------.  .----------------.  .----------------.  .----------------.  .----------------. 
+#| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
+#| |     _____    | || | ____  _____  | || |    _______   | || |  _________   | || |      __      | || |   _____      | || |   _____      | |
+#| |    |_   _|   | || ||_   \|_   _| | || |   /  ___  |  | || | |  _   _  |  | || |     /  \     | || |  |_   _|     | || |  |_   _|     | |
+#| |      | |     | || |  |   \ | |   | || |  |  (__ \_|  | || | |_/ | | \_|  | || |    / /\ \    | || |    | |       | || |    | |       | |
+#| |      | |     | || |  | |\ \| |   | || |   '.___`-.   | || |     | |      | || |   / ____ \   | || |    | |   _   | || |    | |   _   | |
+#| |     _| |_    | || | _| |_\   |_  | || |  |`\____) |  | || |    _| |_     | || | _/ /    \ \_ | || |   _| |__/ |  | || |   _| |__/ |  | |
+#| |    |_____|   | || ||_____|\____| | || |  |_______.'  | || |   |_____|    | || ||____|  |____|| || |  |________|  | || |  |________|  | |
+#| |              | || |              | || |              | || |              | || |              | || |              | || |              | |
+#| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
+# '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' 
+
+
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=6
+BACKTITLE="Node Install Setup Wizard"
+TITLE="Node Install Setup"
+MENU="Choose one of the following coins to install:"
+
+#OPTIONS=(1 "Install Fresh Master Node Already Supported"
+#		 2 "Update Existing Master Node Already Supported"
+#		 3 "Compile Windows Wallet Already Supported"
+#		 4 "Compile a project from Github URL"
+#		 5 "Install Crypto Pool"
+#		 6 "Advanced Install"
+#
+#		 0 "Exit Script"
+#)
+
+#OPTIONS=(1 "Install Fresh Master Node Already Supported"
+#		 2 "Update Existing Master Node Already Supported"
+#		 3 "Compile Windows Wallet Already Supported"
+#		 4 "Compile a project from Github URL"
+#		 5 "Install Crypto Pool"
+#		 6 "Advanced Install"
+#
+#		 0 "Exit Script"
+#)
+
+
+CHOICE=$(whiptail --clear\
+		--backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        0)  # Exit
+		exit	
+		;;
+
+        1)	# Fresh Install
+		bash MasternodeInstall.sh
+        ;; 
+
+		2)	# Update Node
+		bash MasternodeUpdate.sh
+        ;;
+		
+		3)	# Compile Windows Wallet
+		bash CompileWindows.sh
+        ;;
+
+		4)	# Compile From URL
+		cd
+		cd Node_Install/FromURL
+		bash FromURLpick.sh
+        ;;
+
+		5)	#Pool Install
+		cd
+		cd Node_Install/Pool/
+		bash install.sh
+        ;;
+
+		6)	#Advanced Install
+		bash AdvancedInstall.sh
+        ;;
+esac
--- README.md
+++ README.md
@@ -11,51 +11,13 @@ As a warning this will take a good amount of time since you will be compiling yo
 For first time running the this use command bellow:
 
 ```
-git clone https://github.com/MotoAcidic/Node_Install.git && cd Node_Install && bash install.sh
+git clone https://github.com/MotoAcidic/Node_Install.git && cd Node_Install && bash start.sh
 ```
 
 For returning users we will delete this repo and get a clean instance of it to keep it fully up to date:
 
 ```
-rm -rf Node_Install && git clone https://github.com/MotoAcidic/Node_Install.git && cd Node_Install && bash install.sh
-```
-
-## Add A Project
-There is a few areas that need to be added in order for your project to be added to Node Install.
-- Copy one of the coins folder under 
-```
-Node_Install/Coins 
-Example: Node_Install/Altbet
-```
-- Rename that folder to your coin and open the specs file.
-- Edit the specs listed in the example below to your coins specs.
-```
-RPC_PORT=9322
-COIN_PORT=8322
-COIN_NAME='Altbet'
-REPO_NAME='abet'
-COIN_DAEMON='altbetd'
-COIN_CLI='altbet-cli'
-COIN_QT='altbet-qt'
-GITHUB=https://github.com/altbet/abet
-```
-- Add your project to the install option scripts located in:
-```
-Node_Install/Scripts
-```
-Most coins will use the following scripts:
-```
-MasternodeInstall.sh
-MasternodeUpdate.sh
-CompileWindows.sh
-```
-- Add your coins logo to 
-```
-Node_Install/Graphics
-```
-- Add the logo path to the supported coins document located here:
-```
-Node_Install/Docs/README.md
+rm -rf Node_Install && git clone https://github.com/MotoAcidic/Node_Install.git && cd Node_Install && bash start.sh
 ```
 
 ### All documents
